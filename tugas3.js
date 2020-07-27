dbbe5b7bcfae975584d5cc2cd4ddc9b4a0ac57e3
Glitchfer <arif.0496.rahman@gmail.com>
2020-07-27 11:18:54 +0700

    update tugas3

--- tugas3.js
+++ tugas3.js
@@ -1,18 +1,20 @@
 console.log('Jawaban soal No.3');
 console.log('Segitiga siku-suku berupa angka;');
 
-let printSegitiga = 5;
+let printSegitiga = "9";
+if (typeof printSegitiga === "number") {
+  for (printSegitiga; printSegitiga >= 1; printSegitiga--) {
+    var out = '';
+    
+    for (let nilaiLanjutan = 1; nilaiLanjutan <= printSegitiga; nilaiLanjutan++){
+      out = out + nilaiLanjutan;
+    }
 
-for (let nilaiAwal = printSegitiga; nilaiAwal >= 1; nilaiAwal--) {
-  var out = '';
-  
-  for (let nilaiLanjutan = 1; nilaiLanjutan <= nilaiAwal; nilaiLanjutan++){
-    out = out + nilaiLanjutan;
+    console.log(out);
   }
-
-console.log(out);
+} else {
+  console.log("Data harus angka 7oi!");
 }
-
 
 
 console.log('\n');
